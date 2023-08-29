cb2a57693b3e8307bea92d3de5b44bfec458e075
Enzo-CHT <134451832+Enzo-CHT@users.noreply.github.com>
2023-08-29 21:57:33 +1100

    0.2
    UPDATES
    + slidebar.js (affiche la slide selectionnée)
    + getInfos.php
    + style.css (affichage des slides)

--- js/slidebar.js
+++ js/slidebar.js
@@ -2,16 +2,19 @@
 
 
 
-
+showSlide(1);
 
 
 function showSlide(index) {
     var slides = document.querySelectorAll(".slide");
 
+    console.log(slides);
     for (var i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
 
     slides[index-1].style.display = "block";
 }
+
+
 
--- php/getInfos.php
+++ php/getInfos.php
@@ -9,11 +9,11 @@ $json_response = [];
 
 for ($i = 2; $i < sizeof($dir); $i++) {
 
-    $uri = "../projects/" . $dir[$i] ;
-    $json_data = file_get_contents($uri. "/infos.json");
+    $uri = "projects/" . $dir[$i] ;
+    $json_data = file_get_contents("../" .$uri. "/infos.json");
     
     $json_response[] = [
-        'img' => $uri . "img.jpg",
+        'img' => $uri . "/img.jpg",
         'info' => json_decode($json_data, true),
     ];
 
