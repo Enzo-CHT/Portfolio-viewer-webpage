2af517d98827232cf2654e36a07bf8082d66fff5
Enzo-CHT <134451832+Enzo-CHT@users.noreply.github.com>
2023-09-16 23:37:53 +1100

    0.4
    ADD
    + Feuille de style viewpoint.css
    + Feuille de style style.css
    + Fonction d'affichage des informations lié aux images
    
    UPDATES
    + Feuille de style gallery.css

--- index.php
+++ index.php
@@ -7,35 +7,37 @@
   <title>Portfolio</title>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <link rel="stylesheet" href="css/style.css" />
+  <link rel="stylesheet" href="css/gallery.css" />
+  <link rel="stylesheet" href="css/viewpoint.css" />
 </head>
 
 <body>
   <main>
-    <section id="gallery"></section>
-    <input type="button" name="" id="" onclick="showSlide(-1)" value="<" />
-    <input type="button" name="" id="" onclick="showSlide(1)" value=">" />
-    <section id="view">
-      <!--
+    <div>
+      <section id="gallery" class="img-container img-container-width"></section>
+      <input type="button" name="" id="prevBtn" onclick="showSlide(-1)" value="<" />
+      <input type="button" name="" id="nextBtn" onclick="showSlide(1)" value=">" />
+    </div>
+    <section id="infos" class="view view-width">
       <div>
-        <img src="" alt="image-selected-project" id="image-selected-project" />
+        <img class="illustration" src="" alt="image-selected-project" id="image-selected-project" />
       </div>
       <div>
         <h3 id="title-selected-project"></h3>
         <p id="description-selected-project"></p>
         <div>
           <ul class="links-list">
-            <li class="links-element"><a href="#">Github</a></li>
+
           </ul>
         </div>
       </div>
--->
+
     </section>
   </main>
 </body>
 
-<script>
-  fillGallery(
-    // Récupération des infos liés aux projets
+<script type="text/javascript">
+  var publicData = // Récupération des infos liés aux projets
     <?php
     $dir = scandir("projects/");
     $response = array();
@@ -46,8 +48,10 @@
     }
 
     print_r(json_encode($response));
-    ?>
-  );
+    ?>;
+
+
+  fillGallery(publicData);
 
   /**
    * Fonction d'ajout des images dans la section gallery 
@@ -60,6 +64,30 @@
       gallery.innerHTML +=
         "<img class='slide' src='" + element["img"] + "'/>";
     });
+  }
+
+  showInformations(0)
+
+  function showInformations(index) {
+
+    const project = publicData[index];
+    document.getElementById('image-selected-project').src = project['img'];
+    document.getElementById('title-selected-project').innerHTML = project['title'];
+    document.getElementById('description-selected-project').innerHTML = project['description'];
+
+    const linksObject = project['links'];
+    const linksListElement = document.getElementById('links-list');
+   
+    for (const name in linksObject) {
+      if (linksObject.hasOwnProperty(name)) {
+        const value = linksObject[name];
+        const link = "<li class='links-element'><a href='${value}'>${name}</a></li>";
+        linksListElement.innerHTML += link;
+      }
+    }
+
+   
+
   }
 </script>
 <script src="js/slidebar.js"></script>
--- js/slidebar.js
+++ js/slidebar.js
@@ -9,44 +9,49 @@ function showSlide(way = 0) {
   // Ajout d'une fonction de recupération d'info lié au slide ??
   // Lien entre les infos et image ???
 
-  var slides = document.querySelectorAll(".slide,.icon-slide");
+  var slides = document.querySelectorAll(".slide");
 
   var index = 0;
   for (var i = 0; i < slides.length; i++) {
-    if (slides[i].classList.contains("slide")) {
+    if (slides[i].id == "show") {
       index = i;
     }
+    slides[i].id = "";
   }
-
+  
   var next = index + way;
   if (next > slides.length - 1) {
     next = 0;
   } else if (next < 0) {
     next = slides.length - 1;
   }
-
+  
   for (var i = 0; i < slides.length; i++) {
     // Tous les slides sont des icon par défaut
-    slides[i].classList.remove("slide");
+    
     slides[i].classList.remove("left");
     slides[i].classList.remove("right");
     slides[i].classList.remove("centered");
-    slides[i].classList.add("icon-slide");
-
+    slides[i].setAttribute('onclick', 'showInformations('+i+');'); // Add the onclick attribute here
+    
+    
     if (i < next) {
+    
       // Si avant le slide selectionner
       slides[i].classList.add("right");
     } else if (i > next) {
+
       // Si après le slide selectionner
       slides[i].classList.add("left");
     } else {
+
       // Si slide selectionner
-      slides[next].classList.remove("icon-slide");
-      slides[next].classList.add("slide");
+      slides[next].id = "show"
       slides[next].classList.add("centered");
     }
+
+
+
   }
 }
 
-// Affiche les infos lié a l'image  ???
-function updateView(index) {}
